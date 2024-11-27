4acb552327947cade5fbc3b969fe3ff071873483
Veseluxe <114759242+Veseluxe@users.noreply.github.com>
2024-11-27 08:24:47 +0500

    Delete levelcomplete.cs

--- levelcomplete.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class levelcomplete : MonoBehaviour
-{
-  public void LoadNextLevel ()
-  {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
-  }
-}
